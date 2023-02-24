ab2d3358145e036a47571d793ff6d38556ce53f3
Edgar mamikonian <126292498+sSpectrals@users.noreply.github.com>
2023-02-24 21:29:50 +0000

    test

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 // const express = require('express');
 // const app = express();
+// const port = 3000;
 
 // app.get('/', (req, res) => {
-//   res.send('Hello, world!');
+//   res.send('test');
 // });
 
-// const port = process.env.PORT || 3000;
 // app.listen(port, () => {
 //   console.log(`Server running on port ${port}`);
 // });
-
-
-
